4cb2c7f87dc77fd65cbf79439390b89cb81cc372
unknown <artur.pachin@R15207.R-Style.com>
2015-06-23 12:14:17 +0300

    test

--- hello.go
+++ hello.go
@@ -25,11 +25,11 @@ type mzmkApplication struct {
 	CitizenInfo citInfo `json:"citizenInfo"`
 }
 
-type MZMK struct {
+type ApplicationList struct {
 	ApplicationList []mzmkApplication `json:"applicationList"`
 }
 
-const basePath = "/Users/artur/Yandex.Disk/docs/pflb_prj/'15/05.22_Rstyle/mq/"
+const basePath = `c:\Users\Artur.Pachin\Documents\_MSK\06.05_mskAccess\Выпуск0_tkpKSemu\`
 
 var certNum = 0
 
@@ -45,10 +45,11 @@ func mzmkMQh(w http.ResponseWriter, r *http.Request) {
 		//		w.Header().Set("Content-Type", "text/xml;charset=Windows-1251")
 
 		t := time.Now()
-		str := string(content)
+		date := fmt.Sprintf("%02d.%02d.%04d", t.Day(), t.Month(), t.Year()) //dd.mm.yyyy
+
 		//ident := fmt.Sprintf("%02d%02d/%02d", t.Day(), t.Month(), t.Year()%100)  //ddmm/yy
 		num := fmt.Sprintf("%02d%02d%02d/%02d", t.Hour(), t.Minute(), t.Second(), t.Day()) //mmss/hh
-		str = fmt.Sprintf(str, newSnils(), num, num)
+		str := fmt.Sprintf(string(content), newSnils(), date, num, date, num)
 
 		fmt.Fprintf(w, str)
 	}
@@ -56,8 +57,8 @@ func mzmkMQh(w http.ResponseWriter, r *http.Request) {
 }
 
 var givenCerts []GivenCert
-var newMZMKs MZMK
-var newMZRKs MZMK
+var newMZMKs ApplicationList
+var newMZRKs ApplicationList
 
 func mzrkMQh(w http.ResponseWriter, r *http.Request) {
 	filename := path.Join(basePath, "мзркTempl-4.xml")
@@ -67,15 +68,14 @@ func mzrkMQh(w http.ResponseWriter, r *http.Request) {
 		return
 
 	} else {
-		t := time.Now()
-		const format = "%02d%02d/%02d"
-		str := string(content)
-		num := fmt.Sprintf(format, t.Day(), t.Month(), t.Year()%100)   //ddmm/yy
-		ident := fmt.Sprintf(format, t.Minute(), t.Second(), t.Hour()) //mmss/hh
-
-		var cert GivenCert
 
 		if len(givenCerts) > 0 {
+			var cert GivenCert
+			t := time.Now()
+			const format = "%02d%02d/%02d"
+			str := string(content)
+			num := fmt.Sprintf(format, t.Day(), t.Month(), t.Year()%100)   //ddmm/yy
+			ident := fmt.Sprintf(format, t.Minute(), t.Second(), t.Hour()) //mmss/hh
 			cert = givenCerts[0]
 			givenCerts = append(givenCerts[:0], givenCerts[1:]...)
 			str = fmt.Sprintf(str, num, cert.СтраховойНомер, cert.СерияСертификата, cert.НомерСертификата, ident)
@@ -90,7 +90,7 @@ func mzrkMQh(w http.ResponseWriter, r *http.Request) {
 
 func mzmkLoadedh(w http.ResponseWriter, r *http.Request) {
 
-	var mzmk MZMK
+	var mzmk ApplicationList
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(r.Body)
 	b := buf.Bytes()
@@ -106,7 +106,7 @@ func mzmkLoadedh(w http.ResponseWriter, r *http.Request) {
 
 func mzrkLoadedh(w http.ResponseWriter, r *http.Request) {
 
-	var mzmk MZMK
+	var mzmk ApplicationList
 	buf := new(bytes.Buffer)
 	buf.ReadFrom(r.Body)
 	b := buf.Bytes()
@@ -133,10 +133,18 @@ func Testh(w http.ResponseWriter, r *http.Request) {
 
 func newMZMKh(w http.ResponseWriter, r *http.Request) {
 	if len(newMZMKs.ApplicationList) > 0 {
-		m := newMZMKs.ApplicationList[0].CitizenInfo.Snils
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		m := newMZMKs.ApplicationList[0].CitizenInfo
 		newMZMKs.ApplicationList = append(newMZMKs.ApplicationList[:0],
 			newMZMKs.ApplicationList[1:]...)
-		fmt.Fprintf(w,"%d",m)
+
+		b, err := json.Marshal(m)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		}
+
+		fmt.Fprintf(w, "%s", string(b))
 
 	} else {
 		http.Error(w, "Не загружены заявления МЗМК", http.StatusPreconditionFailed)
@@ -147,10 +155,17 @@ func newMZMKh(w http.ResponseWriter, r *http.Request) {
 
 func newMZRKh(w http.ResponseWriter, r *http.Request) {
 	if len(newMZRKs.ApplicationList) > 0 {
-		m := newMZRKs.ApplicationList[0].CitizenInfo.Snils
+		w.Header().Set("Content-Type", "application/json;charset=UTF-8")
+		m := newMZRKs.ApplicationList[0]
 		newMZRKs.ApplicationList = append(newMZRKs.ApplicationList[:0],
 			newMZRKs.ApplicationList[1:]...)
-		fmt.Fprintf(w,"%d",m)
+		b, err := json.Marshal(m)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+
+		}
+
+		fmt.Fprintf(w, "%s", string(b))
 	} else {
 		http.Error(w, "Не загружены заявления МЗРК", http.StatusPreconditionFailed)
 		return
@@ -159,7 +174,7 @@ func newMZRKh(w http.ResponseWriter, r *http.Request) {
 }
 
 type stat struct {
-	MzmksNewPollSize  int
+	MzmksNewPoolSize  int
 	MzrksNewPoolSize  int
 	MzrkReadyPoolSize int
 	LastCert          GivenCert
@@ -168,7 +183,7 @@ type stat struct {
 func statush(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json;charset=UTF-8")
 	var m stat
-	m.MzmksNewPollSize = len(newMZMKs.ApplicationList)
+	m.MzmksNewPoolSize = len(newMZMKs.ApplicationList)
 	m.MzrksNewPoolSize = len(newMZRKs.ApplicationList)
 	m.MzrkReadyPoolSize = len(givenCerts)
 	if m.MzrkReadyPoolSize > 0 {
